e5d365b60ba9a33d19d6e80ac240ca7c2ea4c1b1
UGEcko <vanhelsing314@outlook.com>
2024-04-20 17:03:34 -0400

    Update mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -1,57 +1,63 @@
-import { Wall, Vec3, ColorType,Vec2, worldToWall } from "https://deno.land/x/remapper@3.1.2/src/mod.ts"
+import { Vec3, ColorType,Vec2, Geometry } from "https://deno.land/x/remapper@3.1.2/src/mod.ts"
 
-export class wallGrid {
-    // Individual wall properties
+export class geoGrid {
     time: number
     duration: number
-    color: ColorType = [1,1,1,1]
+    material: GeometryMaterial
     secondaryColor?: ColorType;
-    wallScale: Vec2 = [1,1] // X, Z | Height is calculated by the bounds
-    interactable: boolean = false
+    scale: Vec2 = [1,1] // X/Y, Z | Height is calculated by the bounds
+    lightID?: number;
+    lightType? : number;
     track?: string;
+    track2?:string;
 
     // Effect properties
     grid: Vec2 = [5,5] // X,Y
     position: Vec3 = [0,0,0] // "Offset"
     bounds: Vec2 = [25,25] // X, Y
+    string: any;
 
 
-    constructor(time: number, duration: number) {
+    constructor(time: number, duration: number, material: GeometryMaterial) {
         this.time = time;
         this.duration = duration;
+        this.material = material
     }
+
+
 
     private addVector(vec1: Vec3, vec2: Vec3): Vec3 { // For offsetting position
         return [vec1[0] + vec2[0], vec1[1] + vec2[1], vec1[2] + vec2[2]]
     }
 
-    push(fake: boolean = true) {
+    push() {
         const xInc = this.bounds[0] / this.grid[0];
         const yInc = this.bounds[1] / this.grid[1];
 
-        const wallX = (-(this.bounds[0]/2))
-            for (let x = 0; x < this.grid[0]+1; x++) {
-                const wall = new Wall(this.time, this.duration)
-                const info = worldToWall([wallX+(xInc*x),this.bounds[1]/2,0],[0,0,0],[this.wallScale[0],this.bounds[1],this.wallScale[1]],false)
-                wall.position = [0,0]
-                wall.animate.definitePosition = this.addVector(info.pos, this.position)
-                wall.scale = info.scale
-                wall.color = this.color
-                wall.interactable = this.interactable
-                this.track ? wall.track.value = this.track : undefined;
-                wall.push(fake)
-            }  
-            for (let y = 0; y < this.grid[1]+1; y++) {
-                const wall = new Wall(this.time, this.duration)
-                const info = worldToWall([0,0+(yInc*y),0],[0,0,0],[this.bounds[0],this.wallScale[0],this.wallScale[1]], false)
-                wall.position = [0,0]
-                wall.animate.definitePosition = this.addVector(info.pos, this.position)
-                wall.scale = info.scale
-                wall.color = this.color
-                wall.interactable = this.interactable
-                this.secondaryColor ? wall.color = this.secondaryColor : wall.color = this.color;
-                this.track ? wall.track.value = this.track : undefined;
-                wall.push(fake)
-            }
+        
+        for(let x = 0; x < this.grid[0]+1; x++) {
+            const geo = new Geometry("Cube",this.material)
+            const geoCenter = this.addVector([0,0+(yInc*x),0],[0,-this.bounds[1]/2,0])
+            geo.position = this.addVector(this.position,geoCenter)
+            geo.scale = [this.bounds[0],this.scale[0],this.scale[1]]
+            this.track ? geo.track.value = this.track : undefined
+            this.lightID ? geo.lightID = this.lightID : undefined
+            this.lightType ? geo.lightType = this.lightType : undefined
+            geo.push()
         }
+        for(let y = 0; y < this.grid[1]+1; y++) {
+            const geo = new Geometry("Cube",this.material)
+            const geoCenter = this.addVector([-(this.bounds[0]/2)+(xInc*y),(this.bounds[1]/2),0],[0,-this.bounds[1]/2,0])
+            geo.position = this.addVector(this.position,geoCenter)
+            geo.scale = [this.scale[0],this.bounds[1],this.scale[1]]
+            if(this.track2) {
+                geo.track.value = this.track2
+            } else if(this.track) {
+                geo.track.value = this.track;
+            }
+            this.lightID ? geo.lightID = this.lightID : undefined
+            this.lightType ? geo.lightType = this.lightType : undefined
+            geo.push()
+        }
+    }
 }
